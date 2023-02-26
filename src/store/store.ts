94c7f8dabc12485d5ada182e13470c60bbdd0082
Dima Tatarinov <Nyay@github.com>
2023-02-26 23:16:15 +0100

    feat: added class filter for whole ship list

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,15 @@ import { shipSearchSlice } from '../components/ShipOptions/components/ShipSearch
 import { shipsData } from '../services/shipsData'
 import { shipOptionsSlice } from '../components/ShipOptions/shipOptionsSlice'
 import { nationItemSlice } from '../components/ShipOptions/components/NationFilter/components/NationItem/nationItemSlice'
+import { classItemSlice } from '../components/ShipOptions/components/ClassFilter/components/ClassItem/classItemSlice'
 
 export const store = configureStore({
   reducer: {
     nationSelect: nationItemSlice.reducer,
     shipSearch: shipSearchSlice.reducer,
     setCurrentShip: shipOptionsSlice.reducer,
-    [shipsData.reducerPath]: shipsData.reducer
+    [shipsData.reducerPath]: shipsData.reducer,
+    classSelect: classItemSlice.reducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(shipsData.middleware)
